58b9ef8e1d310688558ece16b26b2ecfa8deaf5a
Andrejus Balciunas <anba8005@gmail.com>
2015-08-07 13:31:13 +0300

    initial

---
+++ VTRWorker.cpp
@@ -0,0 +1,18 @@
+/*
+ * VTRWorker.cpp
+ *
+ *  Created on: Aug 7, 2015
+ *      Author: anba8005
+ */
+
+#include "VTRWorker.h"
+
+VTRWorker::VTRWorker() {
+	// TODO Auto-generated constructor stub
+
+}
+
+VTRWorker::~VTRWorker() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ VTRWorker.h
@@ -0,0 +1,17 @@
+/*
+ * VTRWorker.h
+ *
+ *  Created on: Aug 7, 2015
+ *      Author: anba8005
+ */
+
+#ifndef VTRWORKER_H_
+#define VTRWORKER_H_
+
+class VTRWorker {
+public:
+	VTRWorker();
+	virtual ~VTRWorker();
+};
+
+#endif /* VTRWORKER_H_ */
